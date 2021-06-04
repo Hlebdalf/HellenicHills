beb73f3cecd5db88dc83157fe33a8d9cad066179
Hlebdalf <sarkolin123@gmail.com>
2021-06-04 03:54:55 +0300

    Gravity have turned

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -5,20 +5,30 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-    public int m = 1;
     public float g = 10;
     public float angle = 45;
+    private float forceY;
+    private float forceX;
+    private float step = 0;
     void Start()
     {
         angle = 180 / Mathf.PI * angle;
-        float forceY = -Mathf.Sin(angle) * m * g;
-        float forceX = -Mathf.Cos(angle) * m * g;
-        gameObject.GetComponent<Rigidbody>().AddForce(forceX,forceY, 0);
+        forceY = -Mathf.Sin(angle) * g;
+        forceX = -Mathf.Cos(angle) * g;
+        Physics.gravity = new Vector3(forceX, forceY, 0);
+        //gameObject.GetComponent<Rigidbody>().Fo
+        //gameObject.GetComponent<Rigidbody>().AddForce(forceX,forceY, 0, ForceMode.Impulse );
+        //gameObject.GetComponent<Rigidbody>().AddForce(-forceX, -forceY, 0, ForceMode.Acceleration);
+        //ForceMode myforce = new ForceMode.Ac
+        //gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, g, 0), ForceMode.Acceleration);
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Debug.DrawRay(transform.position, new Vector3(forceX * 100, forceY * 100, 0), Color.red);
+        //Physics.Simulate(step);
+        step += 1f;
     }
 }
