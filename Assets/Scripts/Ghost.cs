11b8f27c005d84d842c8ee1f3eee7a7c9c5adc27
@sarkolin123 <sarkolin123@gmail.com>
2021-06-16 02:08:26 +0300

    Created score record system

--- Assets/Scripts/Ghost.cs
+++ Assets/Scripts/Ghost.cs
@@ -7,14 +7,30 @@ public class Ghost : MonoBehaviour
 {
     public GameObject target;
     public Text scoresText;
+    
     public int scores;
+    private int scoreRecord = 0;
+    private void Awake()
+    {
+        scoreRecord = PlayerPrefs.GetInt("scoreRecord");
+    }
     void Update()
     {
         gameObject.transform.position = target.transform.position;
-        if(gameObject.transform.position.x > scores)
+        if (gameObject.transform.position.x > scores)
         {
             scores = (int)gameObject.transform.position.x;
-            scoresText.text = scores.ToString();
+            scoresText.text = scores.ToString() + "/" + scoreRecord.ToString();
+            if (scores > scoreRecord)
+            {
+                PlayerPrefs.SetInt("scoreRecord", scores);  
+            }
         }
+        //DEBUG TOOL
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerPrefs.SetInt("scoreRecord", 0);
+        }
+        //DEBUG TOOL
     }
 }
