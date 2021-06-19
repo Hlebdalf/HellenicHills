b94535949ad047eb5a3a227e9b7c5c220bc09404
@sarkolin123 <sarkolin123@gmail.com>
2021-06-19 22:36:24 +0300

    Fixed terrain clipping

--- Assets/Scripts/BakingScript.cs
+++ Assets/Scripts/BakingScript.cs
@@ -23,12 +23,15 @@ public class BakingScript : MonoBehaviour
     private List<GameObject> SpruceBatcher = new List<GameObject>();
     private Transform BallTransform;
     public float border = 0;
+    public float koeff = 328;
     private bool isBallExist = false;
     private float spruceHardness = 0.7f;
 
     public float Shift;
+    private float xShift;
     private void Awake()
     {
+        xShift = Shift;
         BallTransform = Ball.GetComponent<Transform>();
         Ball.GetComponent<Transform>().position = new Vector3(-1, 0, Resolution.y);
         seed = Random.Range(-10000f, 10000f);
@@ -39,21 +42,17 @@ public class BakingScript : MonoBehaviour
         StartCoroutine(BuildTerrain());
         gameObject.GetComponent<Camera>().clearFlags = CameraClearFlags.Depth;
     }
-    void Start()
-    {
-        BallTransform.position = new Vector3(Resolution.y / 10, 75, Resolution.x / 2);
-    }
 
     public Texture2D Bake(Vector2 offset)
     {
-        NoiseMaterial.SetFloat("Vector1_2890a1d24f7f415986e2ea5c2f0e3b46", offset.x / 3);////
-        NoiseMaterial.SetFloat("Vector1_fd0d843ba4ac45c2bd344a013bfa0ab7", offset.y + seed);
+        NoiseMaterial.SetFloat("Vector1_2890a1d24f7f415986e2ea5c2f0e3b46", offset.x + seed);
+        NoiseMaterial.SetFloat("Vector1_fd0d843ba4ac45c2bd344a013bfa0ab7", offset.y);
         NoiseMaterial.SetFloat("Vector1_090150e04f634e6eb9f7220d01725be0", seed);
-        RenderTexture renderTexture = RenderTexture.GetTemporary(Resolution.y + 1, Resolution.y + 1);///
+        RenderTexture renderTexture = RenderTexture.GetTemporary(Resolution.y + 1, Resolution.y + 1);
         Graphics.Blit(null, renderTexture, NoiseMaterial);
-        Texture2D texture = new Texture2D(Resolution.y + 1, Resolution.y + 1);///
+        Texture2D texture = new Texture2D(Resolution.y + 1, Resolution.y + 1);
         RenderTexture.active = renderTexture;
-        texture.ReadPixels(new Rect(Vector2.zero, new Vector2Int(Resolution.y + 1, Resolution.y + 1)), 0, 0);///
+        texture.ReadPixels(new Rect(Vector2.zero, new Vector2Int(Resolution.y + 1, Resolution.y + 1)), 0, 0);
         RenderTexture.active = null;
         RenderTexture.ReleaseTemporary(renderTexture);
         return texture;
@@ -78,12 +77,9 @@ public class BakingScript : MonoBehaviour
             StartCoroutine(BuildTerrain());
         }
     }
-    /*private void Update()
-    {
-       
-    }*/
     private void StartGame()
     {
+        BallTransform.position = new Vector3(Resolution.y / 10, 75, Resolution.x / 2);
         canvas.MenuUIActive();
     }
     IEnumerator BuildTerrain()
@@ -99,7 +95,8 @@ public class BakingScript : MonoBehaviour
         Data[2] = Data[0];
         Data[0] = Data[1];
         Data[1] = Data[2];
-        HeightMaps[1] = Bake(new Vector2(X, Z));
+        HeightMaps[1] = Bake(new Vector2(Z * koeff, X * koeff - xShift));
+        xShift += Shift;
         foreach (GameObject it in Spruces[0])
         {
             DestroyImmediate(it, true);
@@ -108,17 +105,17 @@ public class BakingScript : MonoBehaviour
         Spruces[0] = Spruces[1];
         Spruces[1] = new List<GameObject>();
         Texture2D SpruceMap = BakeSpruce();
-        float[,] HeightColors = new float[Resolution.y + 1, Resolution.y + 1]; /////
+        float[,] HeightColors = new float[Resolution.y + 1, Resolution.y + 1];
         for (int p = 0; p < Resolution.y + 1; p++)
         {
             if (p % 5 == 0)
             {
                 yield return null;
             }
-            for (int y = 0; y < Resolution.y + 1; y++) ///
+            for (int y = 0; y < Resolution.y + 1; y++)
             {
-                HeightColors[p, y] = HeightMaps[1].GetPixel(p, y)[0];
-                float SpruceHeight = HeightColors[p, y] + 20;
+                HeightColors[p, y] = HeightMaps[1].GetPixel(p, y)[0]*1.2f;
+                float SpruceHeight = HeightColors[p, y] * 40 + 30;
                 for (int i = 1; i < 4; i++)
                 {
                     if (SpruceMap.GetPixel(i * y, p).r > spruceHardness)
@@ -126,7 +123,7 @@ public class BakingScript : MonoBehaviour
                         if (Random.Range(-10.0f, 10.0f) > 9.2f)
                         {
                             GameObject Charger = Instantiate(RefCharger);
-                            Charger.GetComponent<Transform>().position = new Vector3(X * Resolution.y + p + Resolution.y, SpruceHeight, i * Resolution.y + y + Resolution.y * Z - 2*Resolution.y);
+                            Charger.GetComponent<Transform>().position = new Vector3(X * Resolution.y + p + Resolution.y, SpruceHeight, i * Resolution.y + y + Resolution.y * Z - 2 * Resolution.y);
                             Spruces[1].Add(Charger);
                             Charger.GetComponent<ChargerScript>().death = gameObject.GetComponent<Death>();
                             ChargerBatcher.Add(Charger);
@@ -145,7 +142,7 @@ public class BakingScript : MonoBehaviour
         StaticBatchingUtility.Combine(SpruceBatcher.ToArray(), SpruceRoot);
         StaticBatchingUtility.Combine(ChargerBatcher.ToArray(), ChargerRoot);
         GameObject NewTerrain = Terrain.CreateTerrainGameObject(Data[1]);
-        NewTerrain.GetComponent<Terrain>().terrainData.heightmapResolution = Resolution.y + 1; /////
+        NewTerrain.GetComponent<Terrain>().terrainData.heightmapResolution = Resolution.y + 1;
         NewTerrain.GetComponent<Terrain>().terrainData.SetHeights(0, 0, HeightColors);
         NewTerrain.GetComponent<Terrain>().terrainData.size = new Vector3(Resolution.y, NewTerrain.GetComponent<Terrain>().terrainData.size.y, Resolution.x);
         NewTerrain.GetComponent<Terrain>().materialTemplate = TerrainMaterial;
