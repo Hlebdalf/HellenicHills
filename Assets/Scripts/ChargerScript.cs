efc9d86e3f1ea69ff32092f1c658fe594b9c2a6c
@sarkolin123 <sarkolin123@gmail.com>
2021-06-22 22:21:01 +0300

    Created new field objects without logic; dynamic hardness have been kiled

--- Assets/Scripts/ChargerScript.cs
+++ Assets/Scripts/ChargerScript.cs
@@ -6,17 +6,6 @@ using UnityEngine.UI;
 public class ChargerScript : MonoBehaviour
 {
     public Death death;
-    public GameObject refMarker;
-    public GameObject canvas;
-    private GameObject selfMarker;
-    public GameObject ball;
-    public void StartGame()
-    {
-        selfMarker = Instantiate(refMarker);
-        selfMarker.GetComponent<Transform>().SetParent(canvas.GetComponent<Transform>());
-        selfMarker.GetComponent<RectTransform>().position= Camera.main.WorldToScreenPoint(transform.position);
-        StartCoroutine(MarkerPosition());
-    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.name == "Machine_ball")
@@ -25,17 +14,5 @@ public class ChargerScript : MonoBehaviour
             gameObject.GetComponent<BoxCollider>().enabled = false;
         }
     }
-    IEnumerator MarkerPosition()
-    {
-        while (gameObject.GetComponent<Transform>().position.x > ball.GetComponent<Transform>().position.x + 40)
-        {
-            selfMarker.GetComponent<Image>().color = new Color(0,1,1,1-(gameObject.GetComponent<Transform>().position.x - ball.GetComponent<Transform>().position.x) / 1200);
-            RectTransform nowTransfporm = selfMarker.GetComponent<RectTransform>();
-            nowTransfporm.position = new Vector3(Camera.main.WorldToScreenPoint(transform.position).x, Screen.height - 120, 1);
-            selfMarker.GetComponent<RectTransform>().position = nowTransfporm.position;
-            yield return new WaitForEndOfFrame();
-        }
-        Destroy(selfMarker);
-        yield break;
-    }
+
 }
---
+++ Assets/Scripts/FieldObjMarker.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FieldObjMarker : MonoBehaviour
+{
+    public Death death;
+    public Color color;
+    public GameObject refMarker;
+    public GameObject canvas;
+    private GameObject selfMarker;
+    public GameObject ball;
+    public void StartGame()
+    {
+        selfMarker = Instantiate(refMarker);
+        selfMarker.GetComponent<Transform>().SetParent(canvas.GetComponent<Transform>());
+        selfMarker.GetComponent<RectTransform>().position = Camera.main.WorldToScreenPoint(transform.position);
+        StartCoroutine(MarkerPosition());
+    }
+
+    IEnumerator MarkerPosition()
+    {
+        while (gameObject.GetComponent<Transform>().position.x > ball.GetComponent<Transform>().position.x + 40)
+        {
+            selfMarker.GetComponent<Image>().color = new Color(color.r, color.g, color.b, 1 - (gameObject.GetComponent<Transform>().position.x - ball.GetComponent<Transform>().position.x) / 1200);
+            RectTransform nowTransfporm = selfMarker.GetComponent<RectTransform>();
+            nowTransfporm.position = new Vector3(Camera.main.WorldToScreenPoint(transform.position).x, Screen.height - 120, 1);
+            selfMarker.GetComponent<RectTransform>().position = nowTransfporm.position;
+            yield return new WaitForEndOfFrame();
+        }
+        Destroy(selfMarker);
+        yield break;
+    }
+}
