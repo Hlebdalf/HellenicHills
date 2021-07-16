6ffe6cd4560b719914c297e8508e7851f65a184b
@sarkolin123 <sarkolin123@gmail.com>
2021-07-16 18:59:20 +0300

    Created color grading

--- Assets/Scripts/SunRotation.cs
+++ Assets/Scripts/SunRotation.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class SunRotation : MonoBehaviour
 {
+    public Gradient SunColor;
+    public Gradient FogColorUp;
+    public Gradient FogColorDown;
+    public Gradient SunStrenght;
+    public Gradient GorizontColorUp;
+    public Gradient SkyColorUp;
+    public Gradient GorizontColorDown;
+    public Gradient SkyColorDown;
     public GameObject ball;
-    public GameObject AngleCube;
     public float speed;
     public int rate = 30;
     private int myRate;
@@ -16,9 +23,13 @@ public class SunRotation : MonoBehaviour
     private Light sunLight;
     private Light ballLight;
     private float angle;
-
+    private float angle1;
+    [SerializeField]
+    private bool upOrDown = true;
+    private Vector3 up;
     private void Start()
     {
+        up = transform.forward;
         sun = transform.GetChild(0).gameObject;
         moon = transform.GetChild(1).gameObject;
         sunLight = sun.GetComponent<Light>();
@@ -26,35 +37,51 @@ public class SunRotation : MonoBehaviour
         ballLight = ball.GetComponent<Light>();
         myRate = rate;
         StartCoroutine(RotatorCoroutine());
-    }
 
+    }
 
     private IEnumerator RotatorCoroutine()
     {
         while (true)
         {
-            angle = Quaternion.Angle(transform.rotation, AngleCube.transform.rotation) / 180;
-            if (angle < 0.5f)
+            angle = Quaternion.Angle(transform.rotation, new Quaternion(0, 0, 0, 1)) / 180;
+            angle1 = -Vector3.SignedAngle(up, transform.up, transform.right) / 180;
+            if (angle < 0.5f && upOrDown)
             {
+                upOrDown = false;
                 sun.SetActive(false);
                 moon.SetActive(true);
                 ball.SetActive(true);
-                moonLight.intensity = (1 - angle)/4;
-                ballLight.intensity = (1 - angle - 0.5f)*2;
-                
             }
-            else
+
+            else if (angle > 0.5f && !upOrDown)
             {
                 ball.SetActive(false);
                 sun.SetActive(true);
                 moon.SetActive(false);
-                sunLight.intensity = angle/1.2f;
+                upOrDown = true;
             }
-            backGround = new Color(angle,angle,angle);
+            if (upOrDown)
+            {
+                sunLight.intensity = SunStrenght.Evaluate(angle1).g;
+                sunLight.color = SunColor.Evaluate(angle1);
+                RenderSettings.ambientSkyColor = SkyColorUp.Evaluate(angle1);
+                RenderSettings.ambientEquatorColor = GorizontColorUp.Evaluate(angle1);
+                Camera.main.backgroundColor = FogColorUp.Evaluate(angle1);
+                RenderSettings.fogColor = FogColorUp.Evaluate(angle1);
+            }
+
+            else
+            {
+                moonLight.intensity = (1 - angle) / 4;
+                ballLight.intensity = (1 - angle - 0.5f) * 2;
+                RenderSettings.ambientSkyColor = SkyColorDown.Evaluate(angle1 + 1);
+                RenderSettings.ambientEquatorColor = GorizontColorDown.Evaluate(angle1 + 1);
+                Camera.main.backgroundColor = FogColorDown.Evaluate(angle1 + 1);
+                RenderSettings.fogColor = FogColorDown.Evaluate(angle1 + 1);
+            }
+            backGround = new Color(angle, angle, angle);
             transform.Rotate(speed, 0, 0);
-            Camera.main.backgroundColor = backGround;
-            RenderSettings.fogColor = backGround;
-            RenderSettings.ambientSkyColor = backGround;
             yield return new WaitForSeconds(1 / myRate);
         }
     }
