9f836a5c9c889936550c40e95bd8f9e9bf500dfa
@sarkolin123 <sarkolin123@gmail.com>
2021-07-24 16:17:17 +0300

    Created clamp

--- Assets/Scripts/MagazineScroller.cs
+++ Assets/Scripts/MagazineScroller.cs
@@ -8,22 +8,32 @@ public class MagazineScroller : MonoBehaviour, IBeginDragHandler, IEndDragHandle
 {
     private RectTransform rt;
     private float nowx = 0, prex = 0, deltax = 0;
+    private int childCNT;
     void Start()
     {
         rt = gameObject.GetComponent<RectTransform>();
+        childCNT = transform.GetChildCount();
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
         nowx = eventData.pointerCurrentRaycast.screenPosition.x;
-        prex = nowx;
+        prex = nowx; 
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         nowx = eventData.pointerCurrentRaycast.screenPosition.x;
         deltax = nowx - prex;
-        rt.position = new Vector3(rt.position.x+deltax, rt.position.y, rt.position.z);
+        rt.position = new Vector3(rt.position.x + deltax, rt.position.y, rt.position.z);
         prex = nowx;
+        if (rt.position.x > 0)
+        {
+            rt.position = new Vector3(0, rt.position.y, rt.position.z);
+        }
+        else if (rt.position.x < -(childCNT - 1) * Screen.width)
+        {
+            rt.position =  new Vector3(-(childCNT - 1) * Screen.width, rt.position.y, rt.position.z);
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
