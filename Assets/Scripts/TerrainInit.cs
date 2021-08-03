ba76e01ee41e283704272a4eb470a08d397c9163
@sarkolin123 <sarkolin123@gmail.com>
2021-08-04 01:02:38 +0300

    Fixed memory leak

--- Assets/Scripts/TerrainInit.cs
+++ Assets/Scripts/TerrainInit.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainInit : MonoBehaviour
-{  
+{
+    private Texture2D texture2;
+    private Texture2D texture;
     public void InitTerrain(Material noiseMaterial, Material spruceMaterial, Material refMaterial,
         Vector2Int Resolution, Vector2Int offset, float seed, int koeff, GameObject refFO)
     {
@@ -11,7 +13,7 @@ public class TerrainInit : MonoBehaviour
         noiseMaterial.SetFloat("Vector1_fd0d843ba4ac45c2bd344a013bfa0ab7", offset.y + offset.y * (1 / (Resolution.y + 1)));
         RenderTexture renderTexture = RenderTexture.GetTemporary(Resolution.y + 1, Resolution.y + 1);
         Graphics.Blit(null, renderTexture, noiseMaterial);
-        Texture2D texture = new Texture2D(Resolution.y + 1, Resolution.y + 1);
+        texture = new Texture2D(Resolution.y + 1, Resolution.y + 1);
         RenderTexture.active = renderTexture;
         texture.ReadPixels(new Rect(Vector2.zero, new Vector2Int(Resolution.y + 1, Resolution.y + 1)), 0, 0);
         RenderTexture.active = null;
@@ -33,7 +35,7 @@ public class TerrainInit : MonoBehaviour
         gameObject.GetComponent<Terrain>().heightmapPixelError = 30;
         gameObject.GetComponent<Terrain>().terrainData.size = new Vector3(Resolution.x * koeff, 100, Resolution.x * koeff);
         gameObject.GetComponent<Terrain>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
-        Texture2D texture2 = new Texture2D(Resolution.y + 1, Resolution.y + 1);
+        texture2 = new Texture2D(Resolution.y + 1, Resolution.y + 1);
         Graphics.Blit(null, renderTexture, spruceMaterial);
         RenderTexture.active = renderTexture;
         texture2.ReadPixels(new Rect(Vector2.zero, new Vector2Int(Resolution.x * koeff, Resolution.y * koeff)), 0, 0);
@@ -55,8 +57,13 @@ public class TerrainInit : MonoBehaviour
         }
 
         gameObject.GetComponent<Terrain>().materialTemplate.mainTexture = texture;
-        //DestroyImmediate(texture2, true);
-        //DestroyImmediate(texture, true);
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log('0');
+        DestroyImmediate(texture2, true);
+        DestroyImmediate(texture, true);
     }
 
 }
