64b534e6c58f2054ba4d7f5bb45ba8fbc45287d7
Nikolay Egorov <69803518+Hlebdalf@users.noreply.github.com>
2021-09-19 01:36:58 +0300

    Appended repair model

---
+++ Assets/Scripts/InterFO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterFO : MonoBehaviour
+{
+    private Vector3 up;
+    void Start()
+    {
+        up = transform.up;
+        float childsCNT = (float)transform.childCount;
+        float coin = Random.value;
+        transform.LookAt(transform.parent.GetComponent<FieldObject>().normal + transform.position- up);
+    }
+}
