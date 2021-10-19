5405d86f7339e566c4826613ae8cb5f434778601
Nikolay Egorov <69803518+Hlebdalf@users.noreply.github.com>
2021-10-19 21:08:33 +0300

    Appended screen res optimize

--- Assets/Scripts/FPS.cs
+++ Assets/Scripts/FPS.cs
@@ -7,6 +7,8 @@ public class FPS : MonoBehaviour
 {
     private void Start()
     {
+        int screenHeight = (Screen.height * 1080) / Screen.width;
+        Screen.SetResolution(1080, screenHeight, true);
         Application.targetFrameRate = int.Parse(PlayerPrefs.GetString("Freq", "60"));
     }
     void Update()
