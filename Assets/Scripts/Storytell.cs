bcf13ca712fd7aebb4c87639530576d386185cb7
Nikolay Egorov <69803518+Hlebdalf@users.noreply.github.com>
2021-10-29 22:48:48 +0300

    Created fullstory save system and fixed gradient bug

--- Assets/Scripts/Storytell.cs
+++ Assets/Scripts/Storytell.cs
@@ -9,15 +9,20 @@ public class Storytell : MonoBehaviour
     public float showSpeed = 0.03f;
     public string[] stories;
     public Text storyText;
+    public Text fullStoryText;
     private int progress;
 
     private void Start()
     {
         progress = PlayerPrefs.GetInt("progress", 0);
-    }
+        for (int i = 0; i < progress/2; i++)
+        {
+            fullStoryText.text = fullStoryText.text + stories[i] + '\n';
+        }
+}
     public void UnlockStory()
     {     
-        if (progress > stories.Length) progress = 0;
+        if (progress >= stories.Length * 2) progress = 0;
         if (progress % 2 == 0)
         {
             StartCoroutine(ShowText(stories[(int)progress / 2 + progress % 2]));
@@ -37,7 +42,6 @@ public class Storytell : MonoBehaviour
             storyText.text = str;
             yield return new WaitForSeconds(showSpeed);
         }
-
         yield return new WaitForSeconds(5);
         storyText.text = "";
     }
